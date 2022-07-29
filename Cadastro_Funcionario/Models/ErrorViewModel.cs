5f2026ce216335276e4b5d1762c27e21497ded82
Thiago Gramuglia <thiagogradev@gmail.com>
2022-07-28 21:40:52 -0300

    teste

--- Cadastro_Funcionario/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cadastro_Funcionario.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
